5c2faa3cdcf683ded4c2229faa677c3e7135ef73
themilkpudding <114882101+themilkpudding@users.noreply.github.com>
2025-12-16 22:09:44 +0400

    Кнопка выхода из активной игры + лобби по ширине/высоте экрана

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
-import { IBasePage, PAGES } from '../PageManager';
-
+import { IBasePage} from '../PageManager';
 import GameCanvas from './GameCanvas/GameCanvas';
 import GameMenu from './GameMenu/GameMenu';
+import './Game.scss';
+
 
 const GamePage: React.FC<IBasePage> = (props: IBasePage) => {
     const { setPage } = props;
-    const backClickHandler = () => setPage(PAGES.LOBBY);
 
     return (
         <div className='game'>
-            <h1>Игра</h1>
-            <div className="game-controls">
-                <Button onClick={backClickHandler} text='Назад' />
+            <div className="canvas-container">
+                <h1>Игра</h1>
+                <div className="debug-info">
+                    <p>Управление: WASD - движение, ЛКМ - атака, ПКМ - блок, 1 - меч, 2 - лук, M - магазин</p>
+                </div>
+                <GameCanvas />
             </div>
-            <div className="debug-info">
-                <p>Управление: WASD - движение, ЛКМ - атака, ПКМ - блок, 1 - меч, 2 - лук, M - магазин</p>
-            </div>
-            <GameCanvas />
-            <GameMenu />
+            <GameMenu
+                setPage={setPage}
+            />
         </div>
     );
 };
